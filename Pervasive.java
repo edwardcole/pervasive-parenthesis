7e0cd623243bc3d42ae7188a82421eb1252e7c3f
omocoool <edward.peter.cole@gmail.com>
2024-10-17 11:40:24 -0700

    add validate func
    only checks if parens are valid, should work though
    later add code to check all the characters

--- Pervasive.java
+++ Pervasive.java
@@ -2,7 +2,6 @@ import java.util.ArrayList;
 
 public class Pervasive {
     static ArrayList<Integer> getFactors(int num) {
-        // github test comment
         ArrayList<Integer> factors = new ArrayList<>();
         for (int i = 0; i < num / 2; i++) {
             if (num % i == 0) {
@@ -79,8 +78,17 @@ public class Pervasive {
         }
     }
 
-    static boolean validate() {
-
+    static boolean validate(String str) {
+        int openParens = 0;
+        int closeParens = 0;
+        for (char c : str.toCharArray()) {
+            if (c == '(') {
+                openParens += 1;
+            } else if (c == ')') {
+                closeParens += 1;
+            }
+        }
+        return openParens == closeParens;
     }
 
     static String generate() {
