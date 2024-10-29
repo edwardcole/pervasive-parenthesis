0b76977849ba77f214c9d67b1ac02784b2c04d7f
edwardcole <edward.peter.cole@gmail.com>
2024-10-29 10:18:41 -0700

    merge branches together

--- Tests.java
+++ Tests.java
@@ -1,29 +1,27 @@
 public class Tests {
     static int numCorrect;
-    
-    static void testEvaluate (String expression, int expected){
+
+    static void testEvaluate(String expression, int expected) {
         try {
             Pervasive.evaluateArithmetic(expression);
         } catch (Exception e) {
             System.out.print("⚠️ Exception raised: " + e);
         }
         int actual = Pervasive.evaluateArithmetic(expression);
-        if (actual == expected){
+        if (actual == expected) {
             System.out.print("✅ Test passed");
-        } else{
+        } else {
             System.out.print("⛔ Test failed. Expected: " + expected + "Got: " + actual);
         }
     }
+
     public static void main(String[] args) {
         testEvaluate("(1)3", 5);
         testEvaluate("", 0);
 
     }
-}   
-    static void testInput(String input){
-        
-    }
-    
+}
 
+static void testInput(String input) {
 
-    
+}
