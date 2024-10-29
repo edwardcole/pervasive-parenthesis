8a32032a2fd14556a3850dddddb32c4bfec8e737
edwardcole <edward.peter.cole@gmail.com>
2024-10-29 10:25:01 -0700

    fix some more methods

--- Main.java
+++ Main.java
@@ -4,25 +4,6 @@ public class Main {
 
     public static Scanner scanner = new Scanner(System.in);
 
-    public static int input(String text) {
-        if (text.charAt(0) == 'q') {
-            return -1;
-        }
-        if (text.charAt(0) == 'e')
-            System.out.println("This expression evaluates to: " + Pervasive.evaluate(text));
-        else if (text.charAt(0) == 'g')
-            System.out.println("This number can be generated as: " + Pervasive.generate(text));
-        else if (text.charAt(0) == 'v') {
-            Validation valid = Pervasive.validate(text);
-            System.out.println("The validity of this expression is: " + valid.valid);
-            if (!valid.valid) {
-                System.out.println(valid.invalidationReason);
-            }
-        } else
-            System.out.println("Please enter a valid expression!");
-        return 0;
-    }
-
     // sigby
     public static void main(String[] args) {
         System.out.println("Welcome to Pervasive Parentheses!");
@@ -34,6 +15,23 @@ public class Main {
             if (ret == -1) {
                 break;
             }
+            switch (text.charAt(0)) {
+                case 'q':
+                    break;
+                case 'e':
+                    System.out.println("This expression evaluates to: " + Pervasive.evaluate(text));
+                case 'g':
+                    System.out.println("This number can be generated as: " + Pervasive.generate(text));
+                case 'v':
+                    Validation valid = Pervasive.validate(text);
+                    System.out.println("The validity of this expression is: " + valid.valid);
+                    if (!valid.valid) {
+                        System.out.println(valid.invalidationReason);
+                    }
+                default:
+                    System.out.println("Please enter a valid expression!");
+            }
+
         }
         System.out.println("Thank you for using Pervasive Parentheses!");
     }
