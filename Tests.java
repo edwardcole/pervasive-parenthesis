c948069651f52cbed5b4a5e4f9799883e7586b77
ekim04 <e.kim@universityprep.org>
2024-11-04 14:29:14 -0800

    hhhdrhrd

--- Tests.java
+++ Tests.java
@@ -2,13 +2,14 @@ public class Tests {
     static int numCorrect;
 
     static void testEvaluate(String expression, int expected) {
+        int actual;
         try {
-            Pervasive.evaluate(expression);
+            actual = Pervasive.evaluate(expression);
         } catch (Exception e) {
+            System.out.print("⚠️ Exception raised: " + e);
+            actual = -1;
             System.out.println("⚠️ Exception raised: " + e);
         }
-        int actual = Pervasive.evaluate(expression);
-
         if (actual == expected) {
             System.out.println("✅ Test passed");
         } else {
