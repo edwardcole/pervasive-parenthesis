865b0b6ce189890d4762f0e365377404c02bd90a
ekim04 <e.kim@universityprep.org>
2024-11-07 10:49:09 -0800

    add new tests

--- Tests.java
+++ Tests.java
@@ -11,14 +11,14 @@ public class Tests {
             System.out.println("⚠️ Exception raised: " + e);
         }
         if (actual == expected) {
-            System.out.println("✅ Test passed. " + expected + ", got: " + actual);
+            System.out.println("✅ Test passed");
         } else {
             System.out.println("⛔ Test failed. Expected: " + expected + ", got: " + actual);
         }
     }
 
     static void testValidate(String expression, boolean expected) {
-        Validation actual = new Validation(false, "");
+        Validation actual;
         try {
             actual = Pervasive.validate(expression);
         } catch (Exception e) {
@@ -27,22 +27,21 @@ public class Tests {
             System.out.println("⚠️ Exception raised: " + e);
         }
         if (actual.valid == expected) {
-            System.out.println("✅ Test passed. " + expected + ", got: " + actual);
+            System.out.println("✅ Test passed");
         } else {
-            System.out.println("⛔ Test failed. Expected: " + expected + ", got: " + actual.valid);
+            System.out.println("⛔ Test failed. Expected: " + expected + ", got: " + actual);
         }
+    }
     }
 
     public static void main(String[] args) {
-        // testValidate("e 1(", false);
-        // testValidate("(/)", false);
-        // testValidate("", true);
-        // testValidate("(1)3", true);
-        // testValidate("(()1)", false);
-        // testValidate("(((((1)))))", false);
-
-        testEvaluate("e 1(2(3(4(5)6)7)8)9", 230);
-        testEvaluate("e (((((5)))))", 160);
-        testEvaluate("(1)(2)(3)", 12);
+        testValidate("e 1(", false);
+        testValidate("(/)", false);
+        testValidate("", true);
+        testValidate("(1)3", true);
+        testValidate(")1("), false);
+        testValidate("(()1)", false);
+        testValidate("(((((1)))))", false);
+        
     }
-}
+} 
